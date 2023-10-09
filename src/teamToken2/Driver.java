421f72f2f313de1ff00d9e8d05be8e5f19e0763d
nswank1901 <93402347+nswank1901@users.noreply.github.com>
2023-10-09 18:54:39 -0400

    Added hungry method

--- src/teamToken2/Driver.java
+++ src/teamToken2/Driver.java
@@ -5,6 +5,8 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started.");
 
+		Hungry iamhungry = new Hungry();
+		iamhungry.hungry();
 	}
 
 }
---
+++ src/teamToken2/Hungry.java
@@ -0,0 +1,8 @@
+package teamToken2;
+
+public class Hungry {
+	public void hungry(){
+		System.out.println("We are all hungry.");
+	}
+
+}
